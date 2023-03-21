d79d422920be9067b0dd942836905492b52674d8
Morore Kushimo <mororekush@gmail.com>
2023-03-21 23:20:44 +0100

    style update

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 
 /*Search Button Finction*/
-function expandSearch() {
-  var searchBtn = document.querySelector('.search-btn');
-  var searchBar = document.querySelector('.search-bar');
-  searchBar.classList.toggle('active');
-  searchBtn.classList.toggle('active');
-  searchBar.focus();
+function toggleSearch() {
+  var searchForm = document.getElementById("search-form");
+  searchForm.classList.toggle("active");
 }
